57bac9dd4dca705bfa0016b3ec15932a82905b83
Leviathan <Leviathan1995@outlook.com>
2015-07-28 09:06:21 +0800

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -3,16 +3,16 @@
 using namespace std;
 int main()
 {
-	  DFA DFA_each;
-	  DFA_each.InputRegex();
-	  DFA_each.InsertNode();
-	  DFA_each.RegextoPost();
-	  DFA_each.GetEdgeNumber();
-	  DFA_each.Thompson();
-	  DFA_each.NFAtoDFA();
-	  DFA_each.Hopcroft();
-	  DFA_each.InputString();
-	  DFA_each.Match();
+    DFA DFA_each;
+    DFA_each.InputRegex();
+    DFA_each.InsertNode();
+    DFA_each.RegextoPost();
+    DFA_each.GetEdgeNumber();
+    DFA_each.Thompson();
+    DFA_each.NFAtoDFA();
+    DFA_each.Hopcroft();
+    DFA_each.InputString();
+    DFA_each.Match();
     cout<<"quit!        "<<endl;
-	return 0;
+    return 0;
 }
